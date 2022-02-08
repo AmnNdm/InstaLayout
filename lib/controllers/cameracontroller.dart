f398852685f965227a0b2c130e7e58e2adfe6be5
AmnNdm <aymen.nadeem@cloudclinik.com>
2022-02-08 12:13:41 +0500

    fine till here

--- lib/controllers/cameracontroller.dart
+++ lib/controllers/cameracontroller.dart
@@ -25,7 +25,6 @@ class CameraSController extends GetxController {
     print("inside init");
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
         overlays: ([SystemUiOverlay.bottom]));
-    getimagesPath();
     camera = CameraController(cameras[0], ResolutionPreset.max);
     camera!.initialize().then((value) => update());
   }
@@ -99,52 +98,5 @@ class CameraSController extends GetxController {
       print(e);
       return null;
     }
-  }
-
-  // gallery post view
-
-  List<FileModel> files = [];
-  FileModel? selectedModel;
-  String? image;
-  BoxFit fit = BoxFit.cover;
-
-  getimagesPath() async {
-    // Permission permission = Permission.storage;
-    var imagepath = await StoragePath.imagesPath;
-    var images = jsonDecode(imagepath) as List;
-    files = images.map<FileModel>((e) => FileModel.fromJson(e)).toList();
-    print("inside path");
-    if (files != null && files.length > 0) {
-      print("inside files");
-      // setState(() {
-      selectedModel = files[0];
-      image = files[0].files[0];
-      update();
-      // });
-    }
-  }
-
-  getitems() {
-    return files
-        .map((e) => DropdownMenuItem(
-              child: Text(
-                e.folder,
-                style: const TextStyle(color: Colors.black),
-              ),
-              value: e,
-            ))
-        .toList();
-  }
-}
-
-class FileModel {
-  late List<String> files;
-  late String folder;
-
-  FileModel({required this.files, required this.folder});
-
-  FileModel.fromJson(Map<String, dynamic> json) {
-    files = json['files'].cast<String>();
-    folder = json['folderName'];
   }
 }
